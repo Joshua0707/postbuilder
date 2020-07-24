e95cdf0d80014b71539bfb3324e74b2d5e4bd999
Joshua <oladapoodelola@gmail.com>
2020-07-24 01:43:40 +0100

    Updates

---
+++ precache-manifest.1e2a396fe9875d6b023424be25c29e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f21d5476a57d126a9f9",
+    "url": "/Joshua0707/blogbuilder/static/css/main.88e917b6.chunk.css"
+  },
+  {
+    "revision": "7f21d5476a57d126a9f9",
+    "url": "/Joshua0707/blogbuilder/static/js/main.5dced768.chunk.js"
+  },
+  {
+    "revision": "f060db6992589275e56a",
+    "url": "/Joshua0707/blogbuilder/static/js/runtime~main.70dc807a.js"
+  },
+  {
+    "revision": "00294315a5fd1774bed2",
+    "url": "/Joshua0707/blogbuilder/static/css/2.f36fd4fc.chunk.css"
+  },
+  {
+    "revision": "00294315a5fd1774bed2",
+    "url": "/Joshua0707/blogbuilder/static/js/2.dc5bbce3.chunk.js"
+  },
+  {
+    "revision": "cf8db480c756d8317567228f515313a0",
+    "url": "/Joshua0707/blogbuilder/index.html"
+  }
+];
